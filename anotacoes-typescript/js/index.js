0f619689696bea9d896b809c8013b180f530de55
socioputa <litho.contato@gmail.com>
2022-07-19 21:10:42 -0300

    Termino da introdução ao Typescript

--- anotacoes-typescript/js/index.js
+++ anotacoes-typescript/js/index.js
@@ -1,40 +1,37 @@
 "use strict";
-// string, boolean, number, ...
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 let x = 10;
 x = 20;
 console.log(x);
-// inferencia x anotation
 let y = 12;
-let z = 12; //ambos reconhecem que é um number, mas cuidado na hora de usar
-//tipos
+let z = 12;
 let firstName = "Kalil";
 let age = 24;
 const isAdmin = true;
 console.log(firstName);
 console.log(age);
 console.log(isAdmin);
-//String != string  lembrar que objeto é diferente do tipo
-// object
 const myNumbers = [1, 2, 3];
 console.log(myNumbers);
 console.log(myNumbers.length);
 console.log(firstName.toUpperCase());
 myNumbers.push(5);
 console.log(myNumbers);
-//tuplas -> tuple
 let myTuple;
 myTuple = [5, "teste", ["a", "b"]];
 console.log(myTuple);
-//object literals -> {prop: value}
 const user = {
     name: "Kalil",
     age: 24,
 };
 console.log(user);
 console.log(user.name);
-//any -> aceita qualquer tipo
 let a = 0;
-// union type
 let id = "10";
 console.log(id);
 id = 200;
@@ -42,7 +39,6 @@ console.log(id);
 const userId = 10;
 const productId = "00001";
 const shirId = 123;
-//enum
 var Size;
 (function (Size) {
     Size["P"] = "Pequeno";
@@ -54,8 +50,6 @@ const camisa = {
     size: Size.G,
 };
 console.log(camisa);
-// literal types -> quando você coloca um valor, em vez de tipo, chamando a variavel
-//funções
 function sum(a, b) {
     return a + b;
 }
@@ -78,3 +72,79 @@ function greeting(name, greet) {
 }
 greeting("José");
 greeting("José", "Sir");
+function sumNumbers(nums) {
+    return nums.n1 + nums.n2;
+}
+console.log(sumNumbers({ n1: 1, n2: 2 }));
+function multiplyNumbers(nums) {
+    return nums.n1 * nums.n2;
+}
+console.log(multiplyNumbers({ n1: 7, n2: 2 }));
+function doSomething(info) {
+    if (typeof info === "number") {
+        console.log(`O número é ${info}`);
+        return;
+    }
+    console.log("Não foi passado um número");
+}
+doSomething(5);
+doSomething(true);
+function showArrayItems(arr) {
+    arr.forEach((item) => {
+        console.log(`ITEM: ${item}`);
+    });
+}
+const a1 = [1, 2, 3];
+const a2 = ["a", "b", "c"];
+showArrayItems(a1);
+showArrayItems(a2);
+class User {
+    constructor(name, role, isApproved) {
+        this.name = name;
+        this.role = role;
+        this.isApproved = isApproved;
+    }
+}
+const zeca = new User("Zéca", "Admin", true);
+console.log(zeca);
+class Car {
+    constructor(brand, wheels) {
+        this.brand = brand;
+        this.wheels = wheels;
+    }
+    showBrand() {
+        console.log(`A marca do carro é: ${this.brand}`);
+    }
+}
+const fusca = new Car("VW", 4);
+fusca.showBrand();
+class SuperCar extends Car {
+    constructor(brand, wheels, engine) {
+        super(brand, wheels);
+        this.engine = engine;
+    }
+}
+const a4 = new SuperCar("Audi", 4, 2.0);
+console.log(a4);
+a4.showBrand();
+function BaseParamters() {
+    return function (constructor) {
+        return class extends constructor {
+            constructor() {
+                super(...arguments);
+                this.id = Math.random();
+                this.createAt = new Date();
+            }
+        };
+    };
+}
+let Person = class Person {
+    constructor(name) {
+        this.name = name;
+    }
+};
+Person = __decorate([
+    BaseParamters()
+], Person);
+const sam = new Person("Sam");
+console.log(sam);
--- anotacoes-typescript/src/index.ts
+++ anotacoes-typescript/src/index.ts
@@ -114,3 +114,128 @@ function greeting(name: string, greet?: string){ //essa interrogação coloca a 
 
 greeting("José");
 greeting("José", "Sir");
+
+//interfaces
+interface MathFunctionParams {
+    n1: number,
+    n2: number
+}
+
+function sumNumbers(nums: MathFunctionParams) {
+    return nums.n1 + nums.n2;
+}
+
+console.log(sumNumbers({ n1: 1, n2: 2}));
+
+function multiplyNumbers (nums: MathFunctionParams) {
+    return nums.n1 * nums.n2;
+}
+
+console.log(multiplyNumbers({ n1: 7, n2: 2}));
+
+
+//narrowing - checagem de tipo
+function doSomething(info: number | boolean){
+    if(typeof info === "number"){
+        console.log(`O número é ${info}`);
+        return;
+    }
+    console.log("Não foi passado um número");
+}
+
+doSomething(5);
+doSomething(true);
+
+//generics
+function showArrayItems<T>(arr: T[]){
+    arr.forEach((item) => {
+        console.log(`ITEM: ${item}`);
+    });
+}
+
+const a1 = [1, 2, 3];
+const a2 = ["a", "b", "c"];
+
+showArrayItems(a1);
+showArrayItems(a2);
+
+
+//classes
+class User {
+    name
+    role
+    isApproved
+
+    constructor(name: string, role: string, isApproved: boolean) {
+        this.name = name
+        this.role = role
+        this.isApproved = isApproved
+    }
+}
+
+const zeca = new User("Zéca", "Admin", true);
+
+console.log(zeca);
+
+//interfaces em classe
+interface IVehicle{
+    brand: string
+    showBrand(): void
+}
+
+class Car implements IVehicle{
+    brand
+    wheels
+
+    constructor(brand: string, wheels: number){
+        this.brand = brand;
+        this.wheels = wheels;
+    }
+
+    showBrand(): void{
+        console.log(`A marca do carro é: ${this.brand}`);
+    }
+}
+
+const fusca = new Car("VW", 4);
+
+fusca.showBrand();
+
+
+//herança
+class SuperCar extends Car{
+    engine
+
+    constructor(brand: string, wheels: number, engine: number){
+        super(brand, wheels);
+        this.engine = engine;
+    }
+}
+const a4 = new SuperCar("Audi", 4, 2.0);
+
+console.log(a4);
+a4.showBrand();
+
+
+//decorators
+function BaseParamters(){
+    return function <T extends {new (...args: any[]): {}}>(constructor: T) {
+        return class extends constructor {
+            id = Math.random()
+            createAt = new Date();
+        };
+    };
+}
+
+@BaseParamters()
+
+class Person{
+    name
+    constructor(name: string){
+        this.name = name;
+    }
+}
+
+const sam = new Person("Sam");
+
+console.log(sam);
